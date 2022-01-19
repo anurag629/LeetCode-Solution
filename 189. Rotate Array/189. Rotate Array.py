fdecbacea7bb213aedb490e0ea68df658c97a1d5
Anurag verma <78868769+anurag629@users.noreply.github.com>
2022-01-19 14:03:00 +0530

    added 189

---
+++ 189. Rotate Array/189. Rotate Array.py
@@ -0,0 +1,6 @@
+def rotate(self, nums: List[int], k: int) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    for i in range(k):
+        nums.insert(0,nums.pop())
