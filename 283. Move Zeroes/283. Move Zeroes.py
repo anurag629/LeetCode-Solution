39f81f1a6171fd0387cff21db5df7b45c8566209
Anurag verma <78868769+anurag629@users.noreply.github.com>
2022-01-20 07:40:57 +0530

    added 283

---
+++ 283. Move Zeroes/283. Move Zeroes.py
@@ -0,0 +1,8 @@
+def moveZeroes(self, nums: List[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    n = nums.count(0)
+    for i in range(n):
+        nums.remove(0)
+        nums.append(0)  
