bdd3d4e893990c53e25a2891f50e8a0693829799
Anurag verma <78868769+anurag629@users.noreply.github.com>
2022-01-26 14:46:22 +0530

    added 1 java

---
+++ 1. Two Sum/1. Two Sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for(int i = 0 ; i< nums.length ; i++){
+            if( hm.containsKey(target- nums[i]) ){
+                res[1] = i;
+                res[0] = hm.get(target - nums[i]);
+                return res;
+            }
+            hm.put(nums[i] , i);
+        }  
+        return res;
+    }
+}
